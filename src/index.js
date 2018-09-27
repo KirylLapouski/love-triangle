a1a120aacad33b61bce547b3d4748724edadc6ff
Kirill Lapkovsky <lapkovskyk@mail.ru>
2018-09-27 22:07:21 +0300

    done

--- src/index.js
+++ src/index.js
@@ -3,5 +3,26 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let matches = []
+  let result = 0;
+  preferences.map((value,index,array) => {
+    if(value === array[array[array[value-1]-1]-1]){{
+      if(isAlreadyBeen(array[value-1],array[array[value-1]-1],array[array[array[value-1]-1]-1],matches))
+        result++
+    }
+  }
+  })
+  return result
 };
+
+[]
+function isAlreadyBeen(b,c,d,matches){
+if(b === c && c == d && b == d)
+        return
+  if( matches.indexOf(b)===-1 && matches.indexOf(c)===-1 && matches.indexOf(d)===-1 ){
+    matches.push(b)
+    matches.push(c)
+    matches.push(d)
+    return true
+  }
+}
